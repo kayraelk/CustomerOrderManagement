b95faf031e49bf06c671f48b5a90d8fbd1c8f190
Mustafa Kayra <mustafa.kayra@kion.com.tr>
2023-10-02 19:07:12 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        // Bir iş sınıfı başa sınıfları newlemez. Constructor ile Dependency Injection yapılacaktır.
+        //Soyut nesneyle bağıntı eklenecektir. Ne InMemory ne EntityFramework geçecektir.
+
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,67 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product>
+            {
+                new Product{ProductId=1, CategoryId=1, ProductName="Çanak", UnitPrice=12, UnitsInStock=30},
+                new Product{ProductId=2, CategoryId=2, ProductName="Tabak", UnitPrice=12, UnitsInStock=35},
+                new Product{ProductId=3, CategoryId=1, ProductName="Kabak", UnitPrice=15, UnitsInStock=30},
+                new Product{ProductId=4, CategoryId=2, ProductName="Kaymak", UnitPrice=32, UnitsInStock=23},
+                new Product{ProductId=5, CategoryId=1, ProductName="Bıçak", UnitPrice=21, UnitsInStock=12},
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            Product productToDelete = null;
+
+            //Without Linq
+            //foreach(var item in _products)
+            //{
+            //    if(item.ProductId == product.ProductId)
+            //    {
+            //        productToDelete = item;
+            //    }
+            //}
+
+            //LINQ - Language Integrated Query
+            productToDelete = _products.SingleOrDefault(x => x.ProductId == product.ProductId);
+
+            _products.Add(productToDelete);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(x => x.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = _products.SingleOrDefault(x => x.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+    }
+}
