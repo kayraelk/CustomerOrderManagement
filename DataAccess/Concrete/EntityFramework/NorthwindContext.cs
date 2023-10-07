d1bdb886aac4374940f7ae347a6ca7dd2afd3aba
Mustafa Kayra <mustafa.kayra@kion.com.tr>
2023-10-07 17:41:48 +0300

    EntityFramework eklendi. Connectionstring yapılmadı.

---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context: Db tabloları ile proje classlarını bağlar.
+    public class NorthwindContext: DbContext
+    {
+        //Bu metot projenin hangi veritabanıyla ilişkili olduğunun belirtildiği yerdir.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+        }
+    }
+}
