e8dd9088d71206945323453a38ddefa6a83d33e7
Mustafa Kayra <mustafa.kayra@kion.com.tr>
2023-11-20 20:06:52 +0300

    20.11.2023

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -24,7 +24,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if(DateTime.Now.Hour == 18)
+            if(DateTime.Now.Hour == 1)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
